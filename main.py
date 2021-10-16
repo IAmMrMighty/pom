e93f818e0de9e6aaf53157f6242a4d3b6701fa10
Mitch Dyson <59252676+IAmMrMighty@users.noreply.github.com>
2021-10-16 17:49:44 +0200

    First version of pom released!
    Uploaded files: main.py, colors.py

---
+++ main.py
@@ -0,0 +1,304 @@
+import os
+import ctypes
+import time
+import sys
+import traceback
+import getpass
+import shutil
+import pyautogui
+from colors import bcolors
+
+#Check for administrator priviledges
+is_admin = ctypes.windll.shell32.IsUserAnAdmin()
+if is_admin == 0:
+    print("This script requires administrator priviledges")
+    sys.exit()
+
+#Check if running Microsoft Windows Operating System
+def checkos():
+    if os.name == 'nt':
+        main()
+    else:
+        print("This script can't be run on any other operating system than Windows")
+
+#Clear Console
+def clearconsole():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+## VARIABLES
+rtset = 0 #Restorepoint set
+username = getpass.getuser() #Get logged in user
+
+def systemrestorepoint():
+    global rtset
+    try:
+        lock = open("lock.txt") #Check for systemrestorepoint lock 
+        lock.close
+    except IOError: #Creating systemrestorepoint and creating lock. So it doesnt create one every start...
+        os.system(r'powershell.exe -ExecutionPolicy Bypass -Command "Checkpoint-Computer -Description "POM." -RestorePointType "MODIFY_SETTINGS""')
+        with open("lock.txt", 'w') as lock:
+            lock.write('')
+        rtset = 1
+    except FileNotFoundError: #Same
+        os.system(r'powershell.exe -ExecutionPolicy Bypass -Command "Checkpoint-Computer -Description "POM." -RestorePointType "MODIFY_SETTINGS""')
+        with open("lock.txt", 'w') as lock:
+            lock.write('')
+        rtset = 1
+
+def main():
+    clearconsole()
+    global rtsset
+    try:
+        systemrestorepoint()
+        clearconsole()
+        if rtset == 1:
+            print(bcolors.OKGREEN + "System restore point set.")
+            choose()
+        elif rtset == 0:
+            choose()
+
+    #If user interrupts        
+    except KeyboardInterrupt:
+            print("\nScript canceled by user. Exiting.")
+            sys.exit()
+    #If other failure occurs
+    except Exception:
+            traceback.print_exc(file=sys.stdout)
+    
+#Optionmenu
+choice = 0
+def choose():
+    global choice
+    global username
+    try:
+        clearconsole()
+        print("""
+  __  __               _            _                 __  __   _           _       _           
+ |  \/  |             | |          | |               |  \/  | (_)         | |     | |          
+ | \  / |   __ _    __| |   ___    | |__    _   _    | \  / |  _    __ _  | |__   | |_   _   _ 
+ | |\/| |  / _` |  / _` |  / _ \   | '_ \  | | | |   | |\/| | | |  / _` | | '_ \  | __| | | | |
+ | |  | | | (_| | | (_| | |  __/   | |_) | | |_| |   | |  | | | | | (_| | | | | | | |_  | |_| |
+ |_|  |_|  \__,_|  \__,_|  \___|   |_.__/   \__, |   |_|  |_| |_|  \__, | |_| |_|  \__|  \__, |
+                                             __/ |                  __/ |                 __/ |
+                                            |___/                  |___/                 |___/ 
+""")
+## MADE WITH http://www.patorjk.com/software/taag
+
+        print("\n" + bcolors.WARNING + "POM - Performance Optimizer" + bcolors.FAIL + "\n \nThis script will only work on Windows Systems!" + bcolors.OKBLUE + "\n \n[1] Clear only System temporary files \n[2] Clear only Profile temporary files \n[3] Disable SysMain service\n[4] Clear only Prefetch\n[5] Clear Windows Update Downloadsn \n[6] Check Roaming folder size \n[7] Check system drive C \n\n[9] Run everything \n[0] Exit")
+        choice = input("> ")
+        
+        #Cleaning systemtemp
+        if choice == "1":
+            systemtemp = r"C:\Windows\Temp"
+            clearconsole()
+            try:
+                print("Clearing Systems temporary files")
+                shutil.rmtree(systemtemp, ignore_errors=True)
+                print(bcolors.OKGREEN + "\nDONE.")
+                time.sleep(5)
+                choose()
+            except:
+                print(bcolors.FAIL + 'Error while deleting files. \nGoing back to menu')
+                choose()
+
+        #Cleaning profile temp
+        elif choice == "2":
+            profiletemp = "C:\\Users\\" + username + "\\AppData\\Local\\Temp\\"
+            clearconsole()
+            try:
+                print("Clearing profile temporary files")
+                shutil.rmtree(profiletemp, ignore_errors=True)
+                print(bcolors.OKGREEN + "\nDONE.")
+                time.sleep(5)
+                choose()
+            except:
+                print(bcolors.FAIL + 'Error while deleting files. \n Going back to menu')
+                choose()
+
+        #Disabling and stopping sysmain service
+        elif choice == "3":
+            clearconsole()
+            print("Disabling SysMain Service ...")
+            os.system("sc config sysmain start= disabled >nul")
+            time.sleep(1)
+            os.system("net stop SysMain")
+            print(bcolors.OKGREEN + "DONE.")
+            time.sleep(1)
+            choose()
+
+        #Cleaning prefetch
+        elif choice == "4":
+            prefetch = "C:\\Windows\\Prefetch\\"
+            clearconsole()
+            try:
+                print("Clearing prefetch")
+                if os.path.exists(prefetch):
+                    shutil.rmtree(prefetch)
+                    print(bcolors.OKGREEN + "\nDONE.")
+                    os.mkdir(prefetch)
+                    time.sleep(5)
+                    choose()
+                else:
+                    print("Prefetch folder doesn't exist. Creating ...")
+                    os.mkdir(prefetch)
+                    time.sleep(5)
+                    choose()
+
+            except:
+                print(bcolors.FAIL + 'Error while deleting files. \nGoing back to menu')
+                time.sleep(4)
+                choose()
+        
+        #Clear Windows Update Downloads
+        elif choice == "5":
+            softwaredistribution = 'C:\\Windows\\SoftwareDistribution\\Download\\'
+            clearconsole()
+            try:
+                print("Clearing Windows Update downloads")
+                if os.path.exists(softwaredistribution):
+                    shutil.rmtree(softwaredistribution)
+                    os.mkdir(softwaredistribution)
+                    print(bcolors.OKGREEN + "DONE.")
+                    time.sleep(5)
+                    choose()
+                else:
+                    print("Windows Update Cache folder doesnt exist. Creating...")
+                    os.mkdir(softwaredistribution)
+                    time.sleep(5)
+                    choose()
+            except:
+                print(bcolors.FAIL + 'Error occured. \nGoing back to menu')
+                time.sleep(5)
+                choose()
+        
+        #Checking Roaming folder size
+        elif choice == "6":
+            roamingpath = "C:\\Users\\" + username + "\\AppData\\Roaming\\"
+            size = 0
+
+            for path, dirs, files in os.walk(roamingpath):
+                for f in files:
+                    fp = os.path.join(path, f)
+                    size += os.path.getsize(fp)
+            roamingsize = size / 1024
+            roamingsize = roamingsize / 1024
+            roamingsize = round(roamingsize)
+            if roamingsize >= 600:
+                print(bcolors.FAIL + "Roaming folder might be full")
+                print(str(roamingsize) + "MB\\600MB")
+                time.sleep(5)
+                choose()
+            else:
+                print(bcolors.OKGREEN + "Roaming folder is in good standing.")
+                time.sleep(5)
+                choose()
+        
+        elif choice == "7":
+            clearconsole()
+            print("Checking system drive for failures ...")
+            try:
+                os.system("chkdsk c:>nul")
+                os.system("sfc /scannow>nul")
+                time.sleep(5)
+                print(bcolors.OKGREEN + "DONE.")
+                time.sleep(5)
+                choose()
+            except Exception:
+                print(bcolors.FAIL + "Unable to run chkdsk.")
+    
+
+
+    #RUN EVERYTHING!!
+        elif choice == "9":
+            clearconsole()
+            print(bcolors.OKBLUE + "Running everything ...")
+            
+            ##VARIABLES
+            systemtemp = r"C:\Windows\Temp"
+            profiletemp = "C:\\Users\\" + username + "\\AppData\\Local\\Temp\\"
+            prefetch = "C:\\Windows\\Prefetch\\"
+            softwaredistribution = 'C:\\Windows\\SoftwareDistribution\\Download\\'
+            roamingpath = "C:\\Users\\" + username + "\\AppData\\Roaming\\"
+            size = 0
+            
+            #RUN
+            clearconsole()
+            try:
+                print("Clearing Systems temporary files")
+                shutil.rmtree(systemtemp, ignore_errors=True)
+                print(bcolors.OKGREEN + "\nDONE.")
+                time.sleep(5)
+                print("Clearing profile temporary files")
+                shutil.rmtree(profiletemp, ignore_errors=True)
+                print(bcolors.OKGREEN + "\nDONE.")
+                time.sleep(5)
+                print("Disabling SysMain Service ...")
+                os.system("sc config sysmain start= disabled >nul")
+                time.sleep(1)
+                os.system("net stop SysMain")
+                print(bcolors.OKGREEN + "DONE.")
+                time.sleep(1)
+                print("Clearing prefetch")
+                if os.path.exists(prefetch):
+                    shutil.rmtree(prefetch)
+                    print(bcolors.OKGREEN + "\nDONE.")
+                    os.mkdir(prefetch)
+                    time.sleep(5)
+                else:
+                    print("Prefetch folder doesn't exist. Creating ...")
+                    os.mkdir(prefetch)
+                    time.sleep(5)
+                print("Clearing Windows Update downloads")
+                if os.path.exists(softwaredistribution):
+                    shutil.rmtree(softwaredistribution)
+                    os.mkdir(softwaredistribution)
+                    print(bcolors.OKGREEN + "DONE.")
+                else:
+                    print("Windows Update Cache folder doesnt exist. Creating...")
+                    os.mkdir(softwaredistribution)
+                    time.sleep(5)
+                for path, dirs, files in os.walk(roamingpath):
+                    for f in files:
+                        fp = os.path.join(path, f)
+                        size += os.path.getsize(fp)
+                roamingsize = size / 1024
+                roamingsize = roamingsize / 1024
+                roamingsize = round(roamingsize)
+                if roamingsize >= 600:
+                    print(bcolors.FAIL + "Roaming folder might be full")
+                    print(str(roamingsize) + "MB\\600MB")
+                    time.sleep(5)
+                else:
+                    print(bcolors.OKGREEN + "Roaming folder is in good standing.")
+                    time.sleep(5)
+                print("\nChecking system drive for failures [THIS MIGHT TAKE A WHILE] ...")
+                os.system("chkdsk c:>nul")
+                os.system("sfc /scannow>nul")
+                time.sleep(5)
+                print(bcolors.OKGREEN + "DONE.")
+                time.sleep(5)
+                choose()
+
+            except:
+                print(bcolors.FAIL + 'Error while running script. \nGoing back to menu')
+                time.sleep(2)
+                choose()
+
+        elif choice == "0":
+            clearconsole()
+            print("Good bye! Have a nice day and thanks for using my script.")
+            sys.exit()        
+
+    except KeyboardInterrupt:
+        print(bcolors.FAIL + "\nScript canceled by user. Exiting.")
+        sys.exit()
+    except Exception:
+        traceback.print_exc(file=sys.stdout)
+
+#Startup
+if __name__ == "__main__":
+    clearconsole()
+    width = "129"
+    height = "34"
+    os.system("mode 129,34")
+    checkos()
